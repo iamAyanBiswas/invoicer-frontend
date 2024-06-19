368d39bdf6fea2419519e4f4d636c65db24bf55d
Ayan Biswas <147629539+iamAyanBiswas@users.noreply.github.com>
2024-06-19 05:37:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import store from './redux/store.js'
 import { Provider } from 'react-redux'
-
+//
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
